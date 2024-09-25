f9d750dbb8023fdeecc481db4326ddeab996834c
Rakesh-6610 <rakeshgaming12345@gmail.com>
2024-09-25 23:34:51 +0600

    add how to use in keyboard mouse

--- control-mouse-using-keyboard.py
+++ control-mouse-using-keyboard.py
@@ -1,29 +1,28 @@
 import keyboard
 import pyautogui
+from tabulate import tabulate
 
 
 sensitivity = 10
 
-while True:
-    match keyboard.read_key():
-        case "space":
-            break
-        case "up":
-            pyautogui.move(0, -sensitivity)
-            print("moving up")
-        case "down":
-            pyautogui.move(0, sensitivity)
-            print("moving down")
-        case "left":
-            pyautogui.move(-sensitivity, 0)
-            print("moving left")
-        case "right":
-            pyautogui.move(sensitivity, 0)
-            print("moving right")
-        case "[":
-            if not sensitivity <= 0:
-                sensitivity -= 1
-        case "]":
-            sensitivity += 1
-
-    print(keyboard.read_key())
+print("Button control")
+print(tabulate([ ["exit", "space"], ["mouse up", "arrow up"], ["mouse down", "arrow down"], ["mouse left", "arrow left"], ["mouse right", "arrow right"], ["mouse scroll up", "["], ["mouse scroll down", "]"]], headers=["action", "key"], tablefmt="heavy_grid"))
+req = input("Press any key to continue...")
+if req != "esc" or req == "":
+    while True:
+        match keyboard.read_key():
+            case "space":
+                break
+            case "up":
+                pyautogui.move(0, -sensitivity)
+            case "down":
+                pyautogui.move(0, sensitivity)
+            case "left":
+                pyautogui.move(-sensitivity, 0)
+            case "right":
+                pyautogui.move(sensitivity, 0)
+            case "[":
+                if not sensitivity <= 0: 
+                    sensitivity -= 1
+            case "]":
+                sensitivity += 1 
