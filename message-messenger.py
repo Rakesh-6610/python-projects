00dd63d00a48f801116c9d051e7a445eb270afb7
Rakesh-6610 <rakeshgaming12345@gmail.com>
2024-09-27 00:52:49 +0600

    add function to send multiple messages

--- message-messenger.py
+++ message-messenger.py
@@ -7,6 +7,7 @@ import pickle
 import time
 import os
 from dotenv import load_dotenv
+import pyperclip
 
 
 load_dotenv()
@@ -14,7 +15,10 @@ load_dotenv()
 username = os.getenv("EMAIL")
 password = os.getenv("FACEBOOK_PASSWORD")
 recipient = "Rakesh Karmaker"
-message = "Hello there, this message is from python"
+messages = [
+    "Hello there",
+    "this message is just for testing purposes",  
+]
 
 
 driver = webdriver.Chrome()
@@ -35,13 +39,21 @@ time.sleep(2)
 
 
 
-pin_box = driver.find_element( By.ID , "mw-numeric-code-input-prevent-composer-focus-steal")
-pin_box.send_keys(os.getenv("FACEBOOK_PIN"))
-while "Enter your PIN to restore your chat history" in driver.page_source:
-    time.sleep(5)
+# pin_box = driver.find_element( By.ID , "mw-numeric-code-input-prevent-composer-focus-steal")
+# pin_box.send_keys(os.getenv("FACEBOOK_PIN"))
+# while "Enter your PIN to restore your chat history" in driver.page_source:
+#     time.sleep(5)
+
+cancel_box = WebDriverWait(driver, 10).until(
+    EC.presence_of_element_located((By.XPATH, '//div[@aria-label="Close"]'))
+)
+cancel_box.click()
+dont_restore = WebDriverWait(driver, 10).until(
+    EC.presence_of_element_located((By.XPATH, '//div[@aria-label="Don\'t restore messages"]'))
+)
+dont_restore.click()
 
 
-# search_box = driver.find_element( By.XPATH, '//input[@placeholder="Search Messenger"]')
 
 search_box = WebDriverWait(driver, 10).until(
     EC.presence_of_element_located((By.XPATH, '//input[@placeholder="Search Messenger"]'))
@@ -51,20 +63,24 @@ search_box.send_keys(recipient.split()[0])
 search_box.send_keys(Keys.RETURN)
 search_box.send_keys(Keys.RETURN)
 
-# time.sleep(5)
-
-# select_user = driver.find_element(By.XPATH, f'//ul//li[@role="option"]//span[contains(text(), "Rakesh Karmaker")]')
-
 select_user = WebDriverWait(driver, 10).until(
-    EC.presence_of_element_located((By.XPATH, f'//ul//li[@role="option"]//span[contains(text(), "Rakesh Karmaker")]'))
+    EC.presence_of_element_located((By.XPATH, f'//ul//li[@role="option"]//span[contains(text(), "{recipient}")]'))
 )
 select_user.click()
 time.sleep(2)
 
-# Send the message
-message_box = driver.find_element( By.XPATH, '//div[@aria-label="Message"]')
-message_box.send_keys(message)
-message_box.send_keys(Keys.RETURN)
+
+
+    # Send the message
+
+for message in messages:
+    pyperclip.copy(message)
+    message_box = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.XPATH, '//div[@aria-label="Message"]'))
+    )
+    message_box.click()
+    message_box.send_keys(Keys.CONTROL + 'v')
+    message_box.send_keys(Keys.RETURN)
 
 # Close the browser
 time.sleep(5)
